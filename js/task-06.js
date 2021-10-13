c40d9422db7bfcceda807b5f2bda24460bf1a89e
Valentin Sachenko <valentin181988@gmail.com>
2021-10-13 17:57:56 +0300

    tasc-06

---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#validation-input');
+const length = Number.parseInt(input.dataset.length);
+
+input.addEventListener('blur', (event) => {
+
+    if (input.value.length === length) {
+        input.classList.add('valid');
+
+        if (input.classList.contains('invalid')) {
+            input.classList.remove('invalid');
+        }
+    } else {
+        input.classList.add('invalid');
+        
+        if (input.classList.contains('valid')) {
+            input.classList.remove('valid');
+        }
+    }
+});
