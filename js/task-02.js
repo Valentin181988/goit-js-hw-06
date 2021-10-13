106333ebdcb31921ef76a8e60b38b94f173b9b20
Valentin Sachenko <valentin181988@gmail.com>
2021-10-13 13:56:25 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsArray = [];
+
+ingredients.forEach((ingredientName) => {
+  const item = document.createElement('li');
+  item.textContent = ingredientName;
+  item.classList.add("item");
+  console.log(item);
+  ingredientsArray.push(item);
+})
+
+const elements = document.querySelector('#ingredients');
+
+elements.append(...ingredientsArray);
+
+
+
+
+
+
+
