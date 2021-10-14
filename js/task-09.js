b49d8d5b528d7df9357d625b42c613bd36401222
Valentin Sachenko <valentin181988@gmail.com>
2021-10-14 18:44:41 +0300

    last

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const body = document.querySelector('body');
+const colorName = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  colorName.textContent = newColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,10 @@
+Вешаю событие на батоны
+достаю значение из импута
+Создаю функцию createBoxes(amount)
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
