930ebaa64158392872df2f190eba89f7414d75c3
Valentin Sachenko <valentin181988@gmail.com>
2021-10-14 19:03:08 +0300

    last

--- js/task-10.js
+++ js/task-10.js
@@ -1,10 +1,23 @@
-Вешаю событие на батоны
+/* Вешаю событие на батоны
 достаю значение из импута
-Создаю функцию createBoxes(amount)
+Создаю функцию createBoxes(amount) */
+
+const create = document.querySelector('button[data-create]');
+const destroy = document.querySelector('button[data-destroy]');
+const input = document.querySelector('input');
+console.log(destroy)
 
 
+
+/* create.addEventListener('click', () => {
+
+});
+
+destroy.addEventListener('click', () => {
+
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+} */
 
