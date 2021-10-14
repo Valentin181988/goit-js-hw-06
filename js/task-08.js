af5bb28cdb75b290c652ef503625eb6d4d82e2c1
Valentin Sachenko <valentin181988@gmail.com>
2021-10-14 18:20:09 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value.length === 0 || password.value.length === 0) {
+        alert("Все поля должны быть заполнены!");
+    } else {
+        const valueOfInputs = {};
+
+        valueOfInputs[email.name] = email.value;
+        valueOfInputs[password.name] = password.value;
+
+        console.log(valueOfInputs);
+
+        form.reset();
+    }  
+});
